17c269c6963b0cc2e55d0a6a254bbe8f9619384b
Md Mazharul islam <miforbd@gmail.com>
2022-05-19 18:56:35 +0600

    user shows

--- src/pages/Dashboard/MyAppointment.js
+++ src/pages/Dashboard/MyAppointment.js
@@ -1,15 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import auth from '../../firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const MyAppointment = () => {
     const [appointments, setAppointment] = useState([]);
-    const [user] = useAuthState(auth)
+    const [user] = useAuthState(auth);
+    const navigate=useNavigate()
     useEffect(() => {
-        fetch(`http://localhost:5000/booking?patient=${user.email}`)
-            .then(res => res.json())
-            .then(data => setAppointment(data));
-    }, [user])
+        fetch(`http://localhost:5000/booking?patient=${user.email}`,{
+            method:'GET',
+            headers:{
+                'authorization':`Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if(res.status===401||res.status===403){
+                    signOut(auth);
+                    localStorage.removeItem('accessToken')
+                    navigate('/')
+                }
+               return res.json()
+            })
+            .then(data => {
+                setAppointment(data);
+
+            });
+    }, [user,navigate])
     return (
         <div>
             <h2 className='text-2xl mt-5 font-bold'>My All appointment:{appointments.length}</h2>
@@ -29,7 +47,7 @@ const MyAppointment = () => {
                     <tbody>
                         {
                             appointments.map((appointment,index) => 
-                                <tr>
+                                <tr key={index}>
                                     <th>{index+1}</th>
                                     <td>{appointment.patientName}</td>
                                     <td>{appointment.treatment}</td>
--- src/pages/Dashboard/User.js
+++ src/pages/Dashboard/User.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../shared/Loading/Loading'
 
 const User = () => {
+    const {data:users,isLoading}=useQuery('users',()=>fetch('http://localhost:5000/user',{
+        method:'GET',
+        headers:{
+            authorization:`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+    .then(res=>res.json()));
+    
+    
+
+    if(isLoading){
+        return <Loading/>
+    }
     return (
         <div>
-            <h2 className='text-2xl mt-5 font-bold'>User</h2>
+            <h2 className='text-2xl mt-5 font-bold'>User:{users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Make Admin</th>
+                            <th>Remove user</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user,index) => 
+                                <tr key={user._id}>
+                                    <th>{index+1}</th>
+                                    <td>{user.email}</td>
+                                    <td><button class="btn btn-xs btn-success">Admin</button></td>
+                                    <td><button class="btn btn-xs btn-error">Remove user</button></td>
+                                   
+                                  
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
